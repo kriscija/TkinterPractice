d8718b1144b633b11b20634419d9fdea20c5b183
kriscija <kriscija@rose-hulman.edu>
2018-05-08 23:37:36 -0400

    ok

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -30,20 +30,25 @@ def main():
     say_hello_button = ttk.Button(frame1, text = 'say hi')
     say_hello_button.grid()
     # ------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    # Done
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # ------------------------------------------------------------------
-
+    say_hello_button['command'] = lambda: print("Hello")
     # ------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # Done
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # ------------------------------------------------------------------
+    my_entry_box = ttk.Entry(frame1)
+    my_entry_box.grid()
 
+    say_hello_button = ttk.Button(frame1,text='Test for ok')
+    say_hello_button['command'] = lambda: check_for_ok(my_entry_box)
+    say_hello_button.grid()
     # ------------------------------------------------------------------
     # TODO: 7.
     #    -- Put a second Entry on the Frame.
@@ -67,11 +72,33 @@ def main():
     #      s = entry_box.get()
     #      n = int(s)
     ####################################################################
+    my_entry_box2 = ttk.Entry(frame1)
+    my_entry_box2.grid()
 
+    say_hello_button = ttk.Button(frame1, text='string multiply')
+    say_hello_button['command'] = lambda: stringmult(my_entry_box,my_entry_box2)
+    say_hello_button.grid()
     # ------------------------------------------------------------------
     # TODO: 8. As time permits, do other interesting GUI things!
     # ------------------------------------------------------------------
     root.mainloop()
+
+def print_hello():
+    print("Hello")
+
+def check_for_ok(entry_box):
+    contents = entry_box.get()
+    if contents == 'ok':
+        print('Hello')
+    else:
+        print('goodbye')
+def stringmult(entry_box,entry_box2):
+    str1 = entry_box.get()
+    str2 = entry_box2.get()
+    n= int(str2)
+    print(str1*n)
+
+
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
